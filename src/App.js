d2a8b433554fdaedbd3fa70d54c9921fd59aa663
ndo277 <nhat.do277@gmail.com>
2019-07-23 19:25:14 -0700

    Display student data

--- src/App.js
+++ src/App.js
@@ -8,20 +8,39 @@ class App extends React.Component {
     this.state = {
       rawData: []
     };
-    
+
+    this.fetchRawData = this.fetchRawData.bind(this);
+
   }
 
   componentDidMount(){
+   this.fetchRawData();
+  }
+
+  fetchRawData(){
     fetch('https://www.hatchways.io/api/assessment/students')
       .then(res => res.json())
-      .then(data => console.log(data)); //log the data;
+      .then(data => this.setState({ rawData: data }));
+      // .then(() => console.log(this.state.rawData.students));
   }
 
   render() {
+
+    if (!this.state.rawData.students) return null;
+
     return (
       <div className="App">
         <h2>StudentDB</h2>
 
+        <div className="student-index">
+          {this.state.rawData.students.map(student => {
+            return(
+              <li className="student">
+                {student.firstName}
+              </li>
+            )
+          })}
+        </div>
       </div>
     );
   }
