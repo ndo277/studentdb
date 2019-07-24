f58c6aadcd3c08fb6b0c02383aa691f76fa1485c
ndo277 <nhat.do277@gmail.com>
2019-07-23 21:58:49 -0700

    Add text field & handleinput function

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      rawData: []
+      rawData: [],
+      searchQuery: ""
     };
 
     this.fetchRawData = this.fetchRawData.bind(this);
+    this.handleInput = this.handleInput.bind(this);
 
   }
 
@@ -24,6 +26,10 @@ class App extends React.Component {
       .then(data => this.setState({ rawData: data }));
   }
 
+  handleInput(e){
+    this.setState({searchQuery: e.currentTarget.value});
+  }
+
   render() {
 
     if (!this.state.rawData.students) return null;
@@ -32,6 +38,10 @@ class App extends React.Component {
       <div className="App">
 
         <div className="student-index">
+          <div className="name-filter">
+              <input type="text" className="search-field" onChange={this.handleInput}/>
+          </div>
+
           {this.state.rawData.students.map(student => {
             return(
               <Student student={student} key={student.id}/>
